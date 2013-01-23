7a84c25e90a2742f3d64fa86bf0a35fd3a67bef8
John Reading <john@resonancemultimedia.com>
2013-01-23 15:44:19 -0500

    clean export; friendly output

--- svn-export-changes.sh
+++ svn-export-changes.sh
@@ -15,6 +15,14 @@ target_directory=$4
 # if it's a modified directory it's " M" so won't show with this command (good)
 # if it's an added directory it's still "A" so will show with this command (not so good)
 
+# clear out directory first
+rm -R $target_directory
+
+echo -e "\033[38;5;148mCleaning export directory\033[39m"
+
+# Begin export
+echo -e "\033[00;31mExporting files revised between $revision_from and $revision_to to /$target_directory\033[39m"
+
 for line in `svn diff --summarize -r$revision_from:$revision_to $repository | grep "^[AM]"`
 do
     # each line in the above command in the for loop is split into two:
@@ -28,7 +36,8 @@ do
         if [ ! -d $target_directory$filename ]; then
             directory=`dirname $filename`
             mkdir -p $target_directory$directory
-            svn export -r $revision_to $line $target_directory$filename
+            echo "$filename"
+            svn export -qr $revision_to $line $target_directory$filename
         fi
     fi
 done
